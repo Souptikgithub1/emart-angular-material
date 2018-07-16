b0a3762df429de8db94fbeda0e1634af493b2513
Souptik Karmakar <Souptik Karmakar>
2018-07-16 18:00:11 +0530

    new carousels

--- src/app/shop/homepage/homepage.component.ts
+++ src/app/shop/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../services/product/product.service";
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  isMobilesLessThan10kVisible: boolean = false
+  mobilesLessThan10k: Array<object>;
+
+  isTshirtsInLowPriceVisible: boolean = false;
+    tshirtsInLowPrice: Array<object>;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.getMobilesLesthan10k();
+    this.getTshirtsInLowPrice();
   }
 
+  getMobilesLesthan10k(){
+    const queryParams = {verticalId: 4, maxPrice: 1000000, page: 0, size: 12};
+    this.productService.getProducts(queryParams).subscribe(products => {
+      this.mobilesLessThan10k = products.productDetailsBeans;
+      this.isMobilesLessThan10kVisible = true;
+    });
+  }
+
+  getTshirtsInLowPrice(){
+      const queryParams = {verticalId: 9, maxPrice: 70000, page: 0, size: 12};
+      this.productService.getProducts(queryParams).subscribe(products => {
+          this.tshirtsInLowPrice = products.productDetailsBeans;
+          this.isTshirtsInLowPriceVisible = true;
+      });
+  }
 }
