d1e4a2e6939807ea1d54eff94e434f1cd03fce5a
Souptik Karmakar <Souptik Karmakar>
2018-07-20 20:19:25 +0530

    search implementation

--- src/app/shop/utils/filters.ts
+++ src/app/shop/utils/filters.ts
@@ -2,9 +2,11 @@ export class Filters {
     public static FILTERS = [
         {name: 'RAM', values: [
                 {value: '1 GB', text: '1 GB', checked: false},
+                {value: '1.5 GB', text: '1.5 GB', checked: false},
                 {value: '2 GB', text: '2 GB', checked: false},
                 {value: '3 GB', text: '3 GB', checked: false},
                 {value: '4 GB', text: '4 GB', checked: false},
+                {value: '6 GB', text: '6 GB', checked: false},
                 {value: '8 GB', text: '8 GB', checked: false}
             ]
         },
@@ -13,6 +15,12 @@ export class Filters {
                 {value: '32 GB', text: '32 GB', checked: false},
                 {value: '16 GB', text: '16 GB', checked: false},
                 {value: '8 GB', text: '8 GB', checked: false}
+            ]
+        },
+        {name: 'Processor Core', values: [
+                {value: 'Dual Core', text: 'Dual Core', checked: false},
+                {value: 'Quad Core', text: 'Quad Core', checked: false},
+                {value: 'Octa Core', text: 'Octa Core', checked: false}
             ]
         }
 
