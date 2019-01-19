8f1ebac5c7ea3446abcb975280ebb385d600abf1
Souptik Karmakar <Souptik Karmakar>
2019-01-19 16:02:36 +0530

    drawer login

--- src/app/shared/nav-side-menu/nav-side-menu.component.ts
+++ src/app/shared/nav-side-menu/nav-side-menu.component.ts
@@ -1,6 +1,10 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {NavSideMenuService} from "../../shop/services/navSideMenu/nav-side-menu.service";
 import {MatSidenav} from "@angular/material";
+import {Utils} from "../../shop/utils/utils";
+import {BsModalRef, BsModalService} from "ngx-bootstrap";
+import {RegisterSigninModalComponent} from "../../shop/register-signin-modal/register-signin-modal.component";
+import {AuthService} from "angular4-social-login";
 
 declare var jquery: any;
 declare var $ :any;
@@ -17,6 +21,7 @@ export class NavSideMenuComponent implements OnInit {
 
     verticals = [];
     parentCatItemIndex: number = 0;
+    verticalItemIndex: number = 0;
 
     leaves = [];
 
@@ -27,10 +32,17 @@ export class NavSideMenuComponent implements OnInit {
     @ViewChild('leafDrawer')
     leafDrawer: MatSidenav;
 
-    constructor(private navSideMenuService: NavSideMenuService) {}
+
+    isUserLoggedIn: boolean = false;
+    userDetails: object;
+    userFirstName: string;
+    modalRef: BsModalRef;
+    constructor(private navSideMenuService: NavSideMenuService,
+                private authService: AuthService,
+                private bsModalService: BsModalService) {}
 
     ngOnInit() {
-
+        this.getUserDetails();
         this.navSideMenuService.isSideMenuOpenedObservable.subscribe(response => {
             if(!!response){
                 this.drawer.open();
@@ -57,10 +69,31 @@ export class NavSideMenuComponent implements OnInit {
     }
 
     onClickVertical(i){
+        this.verticalItemIndex = i;
         this.leaves = this.categories[this.parentCatItemIndex]['child'][i]['child'];
         this.leafDrawer.toggle();
     }
 
+    openModal(){
+        this.modalRef = this.bsModalService.show(RegisterSigninModalComponent);
+    }
 
+    getUserDetails(){
+        this.userDetails = Utils.getUserFromLocalStorage();
+        console.log(this.userDetails);
+        if(!!this.userDetails){
+            this.userFirstName = this.userDetails['name'].split(' ')[0];
+            this.isUserLoggedIn = true;
+        }else{
+            this.isUserLoggedIn = false;
+        }
+        this.bsModalService.hide(0);
+    }
+
+    logout(){
+        Utils.removeUserFromLocalStorage();
+        this.authService.signOut();
+        location.reload();
+    }
 
 }
